7d27d3ba582114a526dbb17858e24389b21b1582
ruslan <ruslan@MacBook-Pro.local>
2023-08-01 19:28:17 +0600

    Created main app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vvjppv7dh*1o5i0@)u@9_!4ia(ulzfu$m5vqoelukp(^sm#djy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ("*",)
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.User'
