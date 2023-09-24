a59121d9e6cf94d6798a06c24ead864cbd3715c6
Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
2023-09-24 21:23:03 +0600

    Added filters and rputers

--- main/views.py
+++ main/views.py
@@ -1,11 +1,36 @@
+import django_filters
 from rest_framework import viewsets
 from .models import User, Tag, Task
 from .serializers import UserSerializer, TaskSerializer, TagSerializer
 
 
+class UserFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ("name",)
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+    filterset_class = UserFilter
+
+
+class TaskFilter(django_filters.FilterSet):
+    state = django_filters.CharFilter()
+    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
+    author = django_filters.CharFilter(
+        field_name="author__last_name", lookup_expr="icontains"
+    )
+    assignee = django_filters.CharFilter(
+        field_name="author__last_name", lookup_expr="icontains"
+    )
+
+    class Meta:
+        model = Task
+        fields = ("state", "tags", "author", "assignee")
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -15,6 +40,7 @@ class TaskViewSet(viewsets.ModelViewSet):
         .order_by("id")
     )
     serializer_class = TaskSerializer
+    filterset_class = TaskFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,23 +1,16 @@
-"""
-URL configuration for task_manager project.
+from django.urls import path, include
+from rest_framework import routers
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
 from main.admin import task_manager_admin_site
+from main.views import UserViewSet, TaskViewSet, TagViewSet
+
+router = routers.SimpleRouter()
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"tasks", TaskViewSet, basename="tasks")
+router.register(r"tags", TagViewSet, basename="tags")
+
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
+    path("api/", include(router.urls)),
 ]
