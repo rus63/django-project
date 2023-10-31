94f8f176a1a71490473e484bcb53db7c5738b0e6
rus63 <124333283+rus63@users.noreply.github.com>
2023-10-31 21:41:52 +0600

    Feature/add coveralls (#33)
    * Added badge
    
    * Added coveralls
    
    * Added coveralls
    
    * Added rollbar (#26)
    
    * Added rollbar
    
    * Added newrelic (#27)
    
    * Added newrelic
    
    * Added configuraiton (#28)
    
    * Added configuraiton
    
    * Added templates (#29)
    
    * Added templates
    
    * Added mail service (#30)
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
    
    ---------
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
    
    ---------
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
    
    ---------
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
    
    ---------
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
    
    * Update poetry.lock
    
    * Update poetry.lock
    
    * Update docker-compose.yml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update poetry.lock
    
    ---------
    
    Co-authored-by: Ruslan Iliev <ruslan.iliev@dualbootpartners.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![Build Status](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Factions-badge.atrox.dev%2Frus63%2Fdjango-project%2Fbadge%3Fref%3Ddevelop&style=flat)](https://actions-badge.atrox.dev/rus63/django-project/goto?ref=develop)
+[![Coverage Status](https://coveralls.io/repos/github/OpenSourceHelpCommunity/OpenSourceHelpCommunity.github.io/badge.svg?branch=develop)](https://coveralls.io/github/OpenSourceHelpCommunity/OpenSourceHelpCommunity.github.io?branch=develop)
+
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from task_manager.utils import strtobool
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,7 +54,23 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "rollbar.contrib.django.middleware.RollbarNotifierMiddleware",
 ]
+
+ROLLBAR = {
+    'access_token': 'cfad78d13fda4754a921f9931955708e',
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
+
+EMAIL_USE_SSL = strtobool(os.environ.get("EMAIL_USE_SSL", "0"))
+EMAIL_USE_TLS = strtobool(os.environ.get("EMAIL_USE_TLS", "0"))
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_PORT = os.environ["EMAIL_PORT"]
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
