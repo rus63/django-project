f4716069eec89ddd59cc7bc8b503830c4c42af8d
Ruslan Iliev <ruslan.iliev@dualbootpartners.com>
2023-11-02 10:47:16 +0600

    Added service, changed settings, user model, serializer

--- main/models/user.py
+++ main/models/user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from main.services.storage_backends import public_storage
+
 
 class User(AbstractUser):
     class Roles(models.TextChoices):
@@ -13,3 +15,4 @@ class User(AbstractUser):
     )
     date_of_birth = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
+    avatar_picture = models.ImageField(null=True, storage=public_storage)
